6183957540b4c95555af44a28d6d25791fa77c8e
domenicatonziello <domenica.tonziello10@gmail.com>
2022-12-13 15:57:29 +0100

    esercizio 1

--- jsnack1/script.js
+++ jsnack1/script.js
@@ -4,3 +4,27 @@ console.log('JS OK');
 Il software deve chiedere per 10 volte all’utente di inserire un numero. 
 Il programma stampa la somma di tutti i numeri inseriti.
 */
+
+/*
+1. Prendere elementi dal DOM
+2. Chiedere 10 volte all'utente di inserire un numero
+3. Stampare la somma dei numeri inseriti dall'utente
+*/
+// PRENDERE ELEMENTI DAL DOM:
+const finalNumber = document.getElementById('final-number');
+
+// CHIEDERE 10 VOLTE ALL'UTENTE DI INSERIRE UN NUMERO:
+let somma = 0;
+
+for (let i = 0; i < 10 ; i++) {
+    const userNumber = parseInt(prompt('Digita un numero'));
+    if (!userNumber) {
+        alert('digitare carattere numerico');
+        return;
+    }
+    else{
+        somma += userNumber;
+    }
+}
+    
+finalNumber.innerText = 'La somma dei tuoi numeri è:'+ ' ' + somma; 
